e072f18efcefff7b1e98e19646b1e55982c2ec67
Alejandro Forero Cuervo <alefore@gmail.com>
2026-03-16 16:17:39 +0100

    Simplify end user interface.

--- src/mini_svg.py
+++ src/mini_svg.py
@@ -225,83 +225,8 @@ def MoveAndScale(domain: Box, range: Box) -> ShapeTransformer:
   return ShapeTransformer(ComposeTransformers([to_origin, scale, to_output]))
 
 
-class SvgWriter:
-
-  def __init__(self, filename: pathlib.Path, width: float, height: float,
-               styles: list[pathlib.Path]) -> None:
-    self._filename = filename
-    self._box = Box(0, 0, width, height)
-    self._lines = [
-        '<?xml version="1.0" encoding="UTF-8" standalone="no"?>',
-        '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"',
-        '  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
-        f'<svg xmlns:xlink="http://www.w3.org/1999/xlink" width="{width}" height="{height}" viewBox="0 0 {width} {height}" xmlns="http://www.w3.org/2000/svg" version="1.1">',
-    ]
-
-    if styles:
-      self._add_literal("<style>")
-      for path in styles:
-        self._add_literal(path.read_text())
-      self._add_literal('</style>')
-
-  def get_box(self) -> Box:
-    return self._box
-
-  def _get_extra(self, params: ParamsDict) -> str:
-    extra_params = ""
-    for keyword in ['style', 'class', 'transform']:
-      value = params.get(keyword)
-      if value:
-        extra_params += f" {keyword}='{value}'"
-    return extra_params
-
-  def consume(self, shapes: Iterable[Shape]) -> None:
-    for shape in shapes:
-      self._write_shape(shape)
-    with open(self._filename, 'w') as f:
-      f.write("\n".join(self._lines))
-      f.write("</svg>")
-
-  @singledispatchmethod
-  def _write_shape(self, arg: Any) -> None:
-    raise TypeError(f"Cannot handle type {type(arg)}")
-
-  def _add_literal(self, text: str) -> None:
-    self._lines.append(text)
-
-  @_write_shape.register
-  def _(self, line: Line) -> None:
-    extra_params = self._get_extra(line.params)
-    self._add_literal(
-        f'<line x1="{line.x1:.1f}" y1="{line.y1:.1f}" x2="{line.x2:.1f}" y2="{line.y2:.1f}"{extra_params}/>'
-    )
-
-  @_write_shape.register
-  def _(self, rect: Rect) -> None:
-    extra_params = self._get_extra(rect.params)
-    self._add_literal(
-        f'<rect x="{rect.x:.1f}" y="{rect.y:.1f}" width="{rect.w:.1f}" height="{rect.h:.1f}"{extra_params}/>'
-    )
-
-  @_write_shape.register
-  def _(self, circle: Circle) -> None:
-    extra_params = self._get_extra(circle.params)
-    contents = f"circle cx='{circle.cx:.1f}' cy='{circle.cy:.1f}' r='{circle.r:.1f}'{extra_params}"
-    title = circle.params.get("title")
-    if title:
-      self._add_literal(f"<{contents}><title>{title}</title></circle>")
-    else:
-      self._add_literal(f"<{contents}/>")
-
-  @_write_shape.register
-  def _(self, text: Text) -> None:
-    extra_params = self._get_extra(text.params)
-    self._add_literal(
-        f"<text x='{text.x}' y='{text.y}'{extra_params}>{text.text}</text>")
-
-
 @dataclass(frozen=True)
-class PlotTics:
+class PlotTicks:
   values: frozenset[float]
   value_format: str
 
@@ -313,7 +238,7 @@ def value_with_default(value, default):
 
 
 @dataclass(frozen=True)
-class PlotTicsConfig:
+class PlotTicksConfig:
   # List of values where tics should be drawn. If given, all other fields are
   # ignored.
   values: frozenset[float] | None = None
@@ -326,8 +251,8 @@ class PlotTicsConfig:
 
   value_format: str | None = None
 
-  def with_defaults(self, defaults: "PlotTicsConfig") -> "PlotTicsConfig":
-    return PlotTicsConfig(
+  def with_defaults(self, defaults: "PlotTicksConfig") -> "PlotTicksConfig":
+    return PlotTicksConfig(
         values=value_with_default(self.values, defaults.values),
         max_count=value_with_default(self.max_count, defaults.max_count),
         min_distance=value_with_default(self.min_distance,
@@ -380,34 +305,36 @@ class PlotTicsConfig:
       return self.value_format
     return "d" if base > 1 else f".{abs(math.floor(math.log10(base)))}f"
 
-  def build(self, low: float, high: float) -> PlotTics:
+  def build(self, low: float, high: float) -> PlotTicks:
     max_count = value_with_default(self.max_count, 10)
     if max_count <= 0:
-      return PlotTics(values=frozenset(), value_format="ignored")
+      return PlotTicks(values=frozenset(), value_format="ignored")
     if self.values:
       sorted_values = sorted(self.values)
       base = sorted_values[1] - sorted_values[0]
-      return PlotTics(
+      return PlotTicks(
           values=self.values, value_format=self._get_fmt(low, high, base))
     base = self._find_base(low, high)
-    return PlotTics(
+    return PlotTicks(
         values=self._get_values(low, high, base),
         value_format=self._get_fmt(low, high, base))
 
 
 @dataclass(frozen=True, kw_only=True)
 class _XYPlot:
-  x_axis_values: PlotTicsConfig = PlotTicsConfig()
-  y_axis_values: PlotTicsConfig = PlotTicsConfig()
+  domain: Box | None = None
+  output_range: Box | None = None
+  margins: Margins | None = None
+
+  x_axis_values: PlotTicksConfig = PlotTicksConfig()
+  y_axis_values: PlotTicksConfig = PlotTicksConfig()
 
   x_label: str | None = None
   y_label: str | None = None
 
   labels: frozenset[str] = frozenset()
 
-  domain: Box | None = None
-  output_range: Box | None = None
-  margins: Margins | None = None
+  identity_line: bool | None = None
 
   @property
   def transformer(self) -> ShapeTransformer:
@@ -427,7 +354,9 @@ class _XYPlot:
         y_axis_values=self.y_axis_values.with_defaults(defaults.y_axis_values),
         x_label=self.x_label or defaults.x_label,
         y_label=self.y_label or defaults.y_label,
-        labels=self.labels or defaults.labels)
+        labels=self.labels or defaults.labels,
+        identity_line=value_with_default(self.identity_line,
+                                         defaults.identity_line))
 
   def produce(self) -> ShapeStream:
     return (self._draw() | self.transformer) + self._legend()
@@ -479,52 +408,162 @@ class _XYPlot:
     yield vertical_line(self.domain.x1, self.domain.y1, self.domain.y2)
     yield horizontal_line(self.domain.x1, self.domain.x2, self.domain.y1)
 
+    if self.identity_line:
+      clip = min(self.domain.x2, self.domain.y2)
+      yield Line(0, 0, clip, clip, ParamsDict({"style": "stroke: var(--text)"}))
 
-def with_plot_config(func):
-  plot_fields = {f.name for f in fields(_XYPlot)}
 
-  @wraps(func)
-  def wrapper(*args, **kwargs):
-    plot_kwargs = {k: v for k, v in kwargs.items() if k in plot_fields}
-    remaining_kwargs = {k: v for k, v in kwargs.items() if k not in plot_fields}
-    if args and isinstance(args[0], _XYPlot):
-      return func(*args, **remaining_kwargs)
-    config = _XYPlot(**plot_kwargs)
-    if args and not isinstance(args[0], _XYPlot):
-      # Handle `obj.method(...)`. `args` includes `obj`.
-      return func(args[0], config, *args[1:], **remaining_kwargs)
-    return func(config, *args, **remaining_kwargs)
+def with_config(config_class):
+  config_fields = {f.name for f in fields(config_class)}
 
-  return wrapper
+  def decorator(func):
 
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+      relevant_kwargs = {k: v for k, v in kwargs.items() if k in config_fields}
+      remaining_kwargs = {
+          k: v for k, v in kwargs.items() if k not in config_fields
+      }
+      if args and isinstance(args[0], config_class):
+        return func(*args, **remaining_kwargs)
+      config = config_class(**relevant_kwargs)
+      if args and not isinstance(args[0], config_class):
+        # Handle `obj.method(...)`. `args` includes `obj`.
+        return func(args[0], config, *args[1:], **remaining_kwargs)
+      return func(config, *args, **remaining_kwargs)
+
+    return wrapper
+
+  return decorator
+
+
+with_plot_config = with_config(_XYPlot)
+
+
+class ShapeProducer(ABC):
+
+  @abstractmethod
+  def produce(self, plot: _XYPlot) -> ShapeStream:
+    pass
+
+
+@dataclass(frozen=True)
+class SvgWriter:
+
+  output_path: pathlib.Path
+  width: float
+  height: float
+  css: list[pathlib.Path]
+
+  def get_box(self) -> Box:
+    return simple_box(self.width, self.height)
+
+  def _get_extra(self, params: ParamsDict) -> str:
+    extra_params = ""
+    for keyword in ['style', 'class', 'transform']:
+      value = params.get(keyword)
+      if value:
+        extra_params += f" {keyword}='{value}'"
+    return extra_params
+
+  def consume(self, shapes: Iterable[Shape]) -> None:
+    lines = [
+        '<?xml version="1.0" encoding="UTF-8" standalone="no"?>',
+        '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"',
+        '  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">',
+        f'<svg xmlns:xlink="http://www.w3.org/1999/xlink" width="{self.width}" height="{self.height}" viewBox="0 0 {self.width} {self.height}" xmlns="http://www.w3.org/2000/svg" version="1.1">',
+    ]
+
+    if self.css:
+      lines += ["<style>"]
+      for path in self.css:
+        lines += [path.read_text()]
+      lines += ['</style>']
+
+    lines += map(self._write_shape, shapes)
+    lines += ["</svg>"]
+    with open(self.output_path, 'w') as f:
+      f.write("\n".join(lines))
+
+  @singledispatchmethod
+  def _write_shape(self, arg: Any) -> str:
+    raise TypeError(f"Cannot handle type {type(arg)}")
+
+  @_write_shape.register
+  def _(self, line: Line) -> str:
+    extra_params = self._get_extra(line.params)
+    return f'<line x1="{line.x1:.1f}" y1="{line.y1:.1f}" x2="{line.x2:.1f}" y2="{line.y2:.1f}"{extra_params}/>'
+
+  @_write_shape.register
+  def _(self, rect: Rect) -> str:
+    extra_params = self._get_extra(rect.params)
+    return f'<rect x="{rect.x:.1f}" y="{rect.y:.1f}" width="{rect.w:.1f}" height="{rect.h:.1f}"{extra_params}/>'
+
+  @_write_shape.register
+  def _(self, circle: Circle) -> str:
+    extra_params = self._get_extra(circle.params)
+    contents = f"circle cx='{circle.cx:.1f}' cy='{circle.cy:.1f}' r='{circle.r:.1f}'{extra_params}"
+    title = circle.params.get("title")
+    if title:
+      return f"<{contents}><title>{title}</title></circle>"
+    else:
+      return f"<{contents}/>"
+
+  @_write_shape.register
+  def _(self, text: Text) -> str:
+    extra_params = self._get_extra(text.params)
+    return f"<text x='{text.x}' y='{text.y}'{extra_params}>{text.text}</text>"
+
+
+with_svg_writer = with_config(SvgWriter)
 
 DataDict = NewType("DataDict", dict[str, list[tuple[float, float]]])
 
 
-def box_for_points(data_dict: DataDict) -> Box:
-  all_points = [pt for pts in data_dict.values() for pt in pts]
+@dataclass(frozen=True)
+class _Scatterplot(ShapeProducer):
+  data: DataDict
+  domain: Box
+
+  @shape_generator
+  def _draw(self) -> Iterable[Shape]:
+    radius = min(self.domain.width(), self.domain.height()) / 30
+    for key, points in self.data.items():
+      for x, y in points:
+        yield Circle(x, y, radius,
+                     ParamsDict({
+                         "class": key,
+                         "title": f"{key}: ({x}, {y})"
+                     }))
+
+  def produce(self, plot: _XYPlot) -> ShapeStream:
+    plot = plot.with_defaults(
+        _XYPlot(domain=self.domain, labels=frozenset(self.data)))
+    print(plot)
+    return plot.produce() + plot.transformer(self._draw())
+
+
+@with_svg_writer
+@with_plot_config
+def scatterplot(writer: SvgWriter, plot: _XYPlot,
+                data: dict[str, list[tuple[float, float]]]) -> None:
+  all_points = [pt for pts in data.values() for pt in pts]
   all_x = [pt[0] for pt in all_points]
   all_y = [pt[1] for pt in all_points]
-  return Box(min(0, min(all_x)), min(0, min(all_y)), max(all_x), max(all_y))
-
-
-def scatterplot(data_dict: DataDict) -> Iterable[Shape]:
-  box = box_for_points(data_dict)
-  radius = min(box.width(), box.height()) / 30
-  for key, points in data_dict.items():
-    for x, y in points:
-      yield Circle(x, y, radius,
-                   ParamsDict({
-                       "class": key,
-                       "title": f"{key}: ({x}, {y})"
-                   }))
+  writer.consume(
+      _Scatterplot(
+          DataDict(data),
+          domain=Box(
+              min(0, min(all_x)), min(0, min(all_y)), max(all_x),
+              max(all_y))).produce(
+                  plot.with_defaults(_XYPlot(output_range=writer.get_box()))))
 
 
 BinElements = NewType("BinElements", int)
 
 
 @dataclass(frozen=True)
-class _Histogram:
+class _Histogram(ShapeProducer):
   binned_data: dict[str, list[BinElements]]
 
   bin_size: float
@@ -543,22 +582,26 @@ class _Histogram:
               (bin_index + 0.1 + 0.8 * group_index / len(self.binned_data)), 0,
               individual_bin_width, count, ParamsDict({"class": label.lower()}))
 
-  @with_plot_config
-  def produce(self, plot: _XYPlot) -> Iterable[Shape]:
+  def produce(self, plot: _XYPlot) -> ShapeStream:
     bin_count = max(len(bins) for bins in self.binned_data.values())
     plot = plot.with_defaults(
         _XYPlot(
             domain=Box(self.min_value, 0, self.max_value, self.max_count),
             y_label="Histogram",
-            x_axis_values=PlotTicsConfig(
+            x_axis_values=PlotTicksConfig(
                 values=frozenset(self.min_value + i * self.bin_size
                                  for i in range(0, bin_count, 2))),
-            y_axis_values=PlotTicsConfig(min_distance=1),
+            y_axis_values=PlotTicksConfig(min_distance=1),
             labels=frozenset(self.binned_data)))
     return plot.produce() + plot.transformer(self._draw())
 
 
-def histogram_plot(bins: int, data: dict[str, list[float]]) -> _Histogram:
+@with_svg_writer
+@with_plot_config
+def histogram(writer: SvgWriter, plot: _XYPlot, bins: int,
+              data: dict[str, list[float]]) -> None:
+  print(writer)
+  print(plot)
   all_values = [v for obs in data.values() for v in obs]
   min_value, max_value = min(all_values), max(all_values)
   bin_size = (max_value - min_value) / bins
@@ -574,7 +617,10 @@ def histogram_plot(bins: int, data: dict[str, list[float]]) -> _Histogram:
     binned_data[label] = counts
 
   max_count = max(max(bins) for bins in binned_data.values())
-  return _Histogram(binned_data, bin_size, min_value, max_value, max_count)
+  writer.consume(
+      _Histogram(
+          binned_data, bin_size, min_value, max_value, max_count).produce(
+              plot.with_defaults(_XYPlot(output_range=writer.get_box()))))
 
 
 @dataclass(frozen=True)
@@ -622,7 +668,7 @@ class _BoxPlotOne:
 
 
 @dataclass(frozen=True)
-class _BoxPlot:
+class _BoxPlot(ShapeProducer):
 
   data: dict[str, _BoxPlotOne]
   y_min: float
@@ -634,17 +680,21 @@ class _BoxPlot:
         self.data[key].draw(index, plot)
         for index, key in enumerate(sorted(self.data)))
 
-  @with_plot_config
-  def produce(self, plot: _XYPlot) -> Iterable[Shape]:
+  def produce(self, plot: _XYPlot) -> ShapeStream:
     plot = plot.with_defaults(
         _XYPlot(
             domain=Box(-1, math.floor(self.y_min), len(self.data),
                        math.ceil(self.y_max)),
-            x_axis_values=PlotTicsConfig(max_count=0)))
+            x_axis_values=PlotTicksConfig(max_count=0)))
     return plot.produce() + self._draw(plot)
 
 
-def box_plot(raw_data: dict[str, list[float]]):
-  data = {k: _BoxPlotOne.create(k, v) for k, v in raw_data.items()}
-  return _BoxPlot(data, min(d.y_min for d in data.values()),
-                  max(d.y_max for d in data.values()))
+@with_svg_writer
+@with_plot_config
+def boxplot(writer: SvgWriter, plot: _XYPlot, data: dict[str,
+                                                         list[float]]) -> None:
+  box_data = {k: _BoxPlotOne.create(k, v) for k, v in data.items()}
+  writer.consume(
+      _BoxPlot(box_data, min(d.y_min for d in box_data.values()),
+               max(d.y_max for d in box_data.values())).produce(
+                   plot.with_defaults(_XYPlot(output_range=writer.get_box()))))
