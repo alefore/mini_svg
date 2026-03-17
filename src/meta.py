74a66a54a6c77eb06dad6bc986ae689c741215c9
Alejandro Forero Cuervo <alefore@gmail.com>
2026-03-18 00:18:43 +0100

    Add code to populate argparse based on config dataclasses.

--- src/meta.py
+++ src/meta.py
@@ -1,6 +1,9 @@
-from typing import Type, TypeVar, overload, Any, Callable, cast
-from dataclasses import fields
+import argparse
+from dataclasses import MISSING, fields, is_dataclass
 from functools import wraps
+import pathlib
+from typing import Type, TypeVar, overload, Any, Callable, cast, get_origin, get_args, get_type_hints
+from types import UnionType
 
 T = TypeVar("T")
 
@@ -48,3 +51,45 @@ def with_config(
     return wrapper
 
   return decorator
+
+
+def _extend_argparse_field(var_type: Type[Any], name: str, default,
+                           parser) -> None:
+  origin = get_origin(var_type)
+  args = get_args(var_type)
+  match (origin, args):
+    case (list, (list_type,)):
+      parser.add_argument(f"--{name}", type=list_type, action="append")
+    case _ if is_dataclass(var_type):
+      extend_argparse(var_type, parser, f"{name}_")
+    case _ if var_type is frozenset[str]:
+      parser.add_argument(
+          f"--{name}", type=str, default=default, action="append")
+    case _ if var_type is bool:
+      parser.add_argument(
+          f"--{name}", action=argparse.BooleanOptionalAction, default=default)
+    case _ if var_type in [pathlib.Path, float, int, bool, str]:
+      parser.add_argument(f"--{name}", type=var_type, default=default)
+    case _:
+      print(f"Unable to handle parameter {name}: ", var_type)
+
+
+def extend_argparse(config_class: Type[C],
+                    parser: argparse.ArgumentParser,
+                    prefix=''):
+  type_hints = get_type_hints(config_class)
+  for f in fields(cast(Any, config_class)):
+    origin = get_origin(f.type)
+    args = get_args(f.type)
+    name = prefix + f.name
+    if origin is UnionType:
+      filtered_args = [a for a in args if a is not type(None)]
+      assert len(filtered_args) == 1
+      nested_arg = filtered_args[0]
+      _extend_argparse_field(nested_arg, name, f.default, parser)
+    else:
+      actual_type = type_hints.get(f.name, f.type)
+      if f.default is MISSING:
+        _extend_argparse_field(actual_type, name, None, parser)
+      else:
+        _extend_argparse_field(actual_type, name, f.default, parser)
