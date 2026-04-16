d8cda6932f7994ac57ea0b0aa54f250c0f8ef67f
Alejandro Cuervo <alefore@gmail.com>
2026-04-16 18:11:26 +0200

    Add --watch parameter.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
+from abc import ABC
 import argparse
 import collections
+import datetime
 from dataclasses import dataclass
 import json
 import pathlib
-from typing import Any, cast
+from typing import Any, cast, Callable
 
+import file_watcher
 from meta import create_from_json_data
 from mini_svg import boxplot, histogram, lineplot, scatterplot
 from svg_writer import SvgWriter
@@ -36,7 +39,15 @@ def read_functions(
   return data
 
 
-def _boxplot(config_data: Any) -> None:
+def watch(writer: SvgWriter | None, data: pathlib.Path,
+          watcher: file_watcher.FileWatcher) -> None:
+  if writer:
+    for path in writer.css:
+      watcher.add_file(path)
+  watcher.add_file(data)
+
+
+def _boxplot(config_data: Any, watcher: file_watcher.FileWatcher) -> None:
 
   @dataclass(frozen=True)
   class BoxPlot:
@@ -45,10 +56,11 @@ def _boxplot(config_data: Any) -> None:
     data_path: pathlib.Path = pathlib.Path("/dev/stdin")
 
   config = create_from_json_data(BoxPlot, config_data)
+  watch(config.writer, config.data_path, watcher)
   boxplot(config.writer, config.plot, data=read_distributions(config.data_path))
 
 
-def _histogram(config_data: Any) -> None:
+def _histogram(config_data: Any, watcher: file_watcher.FileWatcher) -> None:
 
   @dataclass(frozen=True)
   class Histogram:
@@ -58,6 +70,7 @@ def _histogram(config_data: Any) -> None:
     data_path: pathlib.Path = pathlib.Path("/dev/stdin")
 
   config = create_from_json_data(Histogram, config_data)
+  watch(config.writer, config.data_path, watcher)
   histogram(
       config.writer,
       config.plot,
@@ -65,7 +78,7 @@ def _histogram(config_data: Any) -> None:
       bins=config.bins)
 
 
-def _lineplot(config_data: Any) -> None:
+def _lineplot(config_data: Any, watcher: file_watcher.FileWatcher) -> None:
 
   @dataclass(frozen=True)
   class LinePlot:
@@ -74,10 +87,11 @@ def _lineplot(config_data: Any) -> None:
     data_path: pathlib.Path = pathlib.Path("/dev/stdin")
 
   config = create_from_json_data(LinePlot, config_data)
+  watch(config.writer, config.data_path, watcher)
   lineplot(config.writer, config.plot, read_functions(config.data_path))
 
 
-def _scatterplot(config_data: Any) -> None:
+def _scatterplot(config_data: Any, watcher: file_watcher.FileWatcher) -> None:
 
   @dataclass(frozen=True)
   class Scatterplot:
@@ -89,30 +103,45 @@ def _scatterplot(config_data: Any) -> None:
   scatterplot(config.writer, config.plot, data=read_functions(config.data_path))
 
 
-def main() -> None:
-  parser = argparse.ArgumentParser(description="Generate SVG plots.")
+def generate(config_path: pathlib.Path,
+             watcher: file_watcher.FileWatcher) -> None:
+  watcher.add_file(config_path)
+  try:
+    config_data = json.loads(config_path.read_text())
+  except json.JSONDecodeError as e:
+    raise RuntimeError(f"{config_path}: Invalid JSON in config: {e}")
 
-  parser.add_argument(
-      "config", type=argparse.FileType('r'), help="Path to JSON config.")
-
-  args = parser.parse_args()
-
-  with args.config as f:
-    try:
-      config_data = json.loads(f.read())
-    except json.JSONDecodeError as e:
-      parser.error(f"{f.name}: Invalid JSON in config: {e}")
-
-  HANDLERS = {
+  if len(config_data) != 1:
+    raise RuntimeError(
+        f"{config_path}: Too many entries in config (expected at most 1).")
+  plot_type = list(config_data)[0]
+  HANDLERS: dict[str, Callable[[Any, file_watcher.FileWatcher], None]] = {
       "boxplot": _boxplot,
       "histogram": _histogram,
       "lineplot": _lineplot,
       "scatterplot": _scatterplot
   }
-  if len(config_data) != 1:
-    parser.error(f"{f.name}: Too many entries in config (expected at most 1).")
-  plot_type = list(config_data)[0]
-  HANDLERS[plot_type](config_data[plot_type])
+  HANDLERS[plot_type](config_data[plot_type], watcher)
+
+
+def main() -> None:
+  parser = argparse.ArgumentParser(description="Generate SVG plots.")
+
+  parser.add_argument(
+      "--watch", action="store_true", help="Run daemon watching for changes.")
+  parser.add_argument("config", type=pathlib.Path, help="Path to JSON config.")
+
+  args = parser.parse_args()
+
+  watcher: file_watcher.FileWatcher = (
+      file_watcher.FileWatcherImpl()
+      if args.watch else file_watcher.NullFileWatcher())
+  generate(args.config, watcher)
+  if args.watch:
+    print("Will watch\n")
+    while True:
+      watcher.wait_for_changes()
+      generate(args.config, watcher)
 
 
 if __name__ == "__main__":
