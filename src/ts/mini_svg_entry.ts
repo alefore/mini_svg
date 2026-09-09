97dff50748786ac402345540534c10b7fb7885be
Alejandro Forero Cuervo <alefore@gmail.com>
2026-09-09 23:20:33 +0200

    Export formatDuration.

--- src/ts/mini_svg_entry.ts
+++ src/ts/mini_svg_entry.ts
@@ -1,4 +1,5 @@
 export {Box, Margins} from './box.js';
 export {lineplot} from './mini_svg.js';
 export {SvgWriter} from './svg_writer.js';
+export {formatDuration} from './time_formats.js'
 export {XYPlot} from './xyplot.js';
